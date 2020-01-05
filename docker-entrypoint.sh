41e2795ab4095ad26ce0d552c31dc12c9d80974e
Tristan K <tristanmkernan@gmail.com>
2020-01-05 12:02:27 -0500

    feat: update docker container names, disable static files

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,8 @@
 
 # https://stackoverflow.com/a/33993532
 # Collect static files
-echo "Collect static files"
-python manage.py collectstatic --noinput
+# echo "Collect static files"
+# python manage.py collectstatic --noinput
 
 # Apply database migrations
 echo "Apply database migrations"
